7adff2ea2cedee896510b68dd1079d411a8bc653
Marcozzi96 <agostini.marco.1996@gmail.com>
2025-04-20 01:12:05 +0200

    Inizializzazione prj

---
+++ src/main/java/it/javaWS/javaws/JavawsApplication.java
@@ -0,0 +1,13 @@
+package it.javaWS.javaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/javaWS/javaws/JavawsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.javaWS.javaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
